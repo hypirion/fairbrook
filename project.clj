d1e647fb8a7ed9c4155dd3230672bea4b1b9123d
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-12-15 22:06:28 +0100

    Add url in project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fairbrook "0.1.0-SNAPSHOT"
   :description "Fine-grained map manipulation for the masses."
-;  :url "http://example.com/FIXME"
+  :url "http://github.com/hyPiRion/fairbrook"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
