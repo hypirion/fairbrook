5df8a6c4fd4b40819804a2ff93d022c505d29c5a
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-12-16 12:42:43 +0100

    Put functions in order of generality.

--- src/fairbrook/meta.clj
+++ src/fairbrook/meta.clj
@@ -1,13 +1,21 @@
 (ns fairbrook.meta)
 
-(defn mono-apply
-  "Applies f with more-params and elements on both the data and the metadata of
-  the elements. Returns the result with the resulting metadata attached."
-  {:arglists '([[f & more-params] elements])}
-  [[f & m] elts]
-  (with-meta
-    (apply f (concat m elts))
-    (apply f (concat m (map meta elts)))))
+(defn ff
+  "Takes in two functions of two arguments, and returns a new function which
+  applies the first function to the value of the two arguments given and applies
+  the second function to their metadata. Attaches the newly generated
+  metadata to the new value."
+  [f meta-f]
+  (fn [a b]
+    (with-meta
+      (f a b)
+      (meta-f (meta a) (meta b)))))
+
+(defn fm
+  "Takes in a function of two arguments, and returns the same function which in
+  addition merges their metadata and attaches it to the resulting value."
+  [f]
+  (ff f merge))
 
 (defn duo-apply
   "Applies f with more-params and elements, and meta-f with meta-more-params and
@@ -17,18 +25,14 @@
   [[f & m] [mf & mm] elts]
   (with-meta
     (apply f (concat m elts))
-    (apply mf (concat mm (map elts)))))
+    (apply mf (concat mm (map meta elts)))))
 
-(defn mono-reduce
-  "Performs a reduction on elements and their metadata with f. Returns the
-  result with the resulting metadata attached. If no init value is given, the
-  init value will be an empty map."
-  ([f elements]
-     (mono-reduce f {} elements))
-  ([f init elements]
-     (with-meta
-       (reduce f init elements)
-       (reduce f init (map meta elements)))))
+(defn mono-apply
+  "Applies f with more-params and elements on both the data and the metadata of
+  the elements. Returns the result with the resulting metadata attached."
+  {:arglists '([[f & more-params] elements])}
+  [args elts]
+  (duo-apply args args elts))
 
 (defn duo-reduce
   "Performs a reduction on elements with f, and their metadata with
@@ -40,6 +44,15 @@
      (with-meta
        (reduce f init elements)
        (reduce meta-f meta-init (map meta elements)))))
+
+(defn mono-reduce
+  "Performs a reduction on elements and their metadata with f. Returns the
+  result with the resulting metadata attached. If no init value is given, the
+  init value will be an empty map."
+  ([f elements]
+     (mono-reduce f {} elements))
+  ([f init elements]
+     (duo-reduce f init f init elements)))
 
 (defn meta-merge
   "As merge, but will merge the metadata from each map as well. As with merge,
@@ -58,20 +71,3 @@
    (fn [m1 m2]
      (merge-with f m1 m2))
    maps))
-
-(defn ff-meta
-  "Takes in two function of two arguments, and returns a new function which
-  applies the first function to the value of the two arguments given and applies
-  the second function to their metadata. Attaches the newly generated
-  metadata-map to the new value."
-  [f meta-f]
-  (fn [a b]
-    (with-meta
-      (f a b)
-      (meta-f (meta a) (meta b)))))
-
-(defn fm-meta
-  "Takes in a function of two arguments, and returns the same function which in
-  addition merges their metadata and attaches it to the resulting value."
-  [f]
-  (ff-meta f merge))
