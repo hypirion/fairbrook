30a330ad0d88e30e7a050ebdc9ccc954209b6d03
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-12-18 00:50:52 +0100

    Make guardrails easier to read, add new ff tests.

--- test/fairbrook/test/meta.clj
+++ test/fairbrook/test/meta.clj
@@ -2,13 +2,13 @@
   (:use [clojure.test]
         [fairbrook.meta]))
 
-(defn- right [a b] b)
-(defn- left [a b] a)
+(defn right [a b] b)
+(defn left [a b] a)
 
-(def ^:private a (with-meta {:a 1} {:baz 10, :foo true}))
-(def ^:private b (with-meta {:b 2} {:bar true, :foo false}))
-(def ^:private c (with-meta {:c 3} {:quux [1 2 3], :baz "hello"}))
-(def ^:private d (with-meta {:d 4} {:test true, :foo true}))
+(def a ^{:baz 10, :foo true} {:a 1})
+(def b ^{:bar true, :foo false} {:b 2})
+(def c ^{:quux [1 2 3], :baz "hello"} {:c 3})
+(def d ^{:test true, :foo true} {:d 4})
 
 (deftest test-meta-ff
   (let [rl (ff right left)
@@ -19,17 +19,44 @@
 
         mmerge2 (ff merge merge)
         mmerge2-res (mmerge2 a b)]
-    (testing "that ff applies data-fn correctly"
+    (testing "that ff applies data-fn correctly for two args"
       (are [actual expected] (= actual expected)
            lr-res a
            rl-res b
            mmerge2-res (merge a b)))
 
-    (testing "that ff applies metadata-fn correctly"
+    (testing "that ff applies metadata-fn correctly for two args"
       (are [actual expected] (= (meta actual) expected)
            lr-res (meta b)
            rl-res (meta a)
-           mmerge2-res (merge (meta a) (meta b))))))
+           mmerge2-res (merge (meta a) (meta b)))))
+  (let [conc-empty (ff concat (constantly {}))
+        conc-empty-res (conc-empty ^{:foo :bar}             [:a :b :c]
+                                   ^{:baz 176}              [1 42 187]
+                                   ^{:spam 10 :single true} [5])
+
+        union-+ (ff (fn [& args] (reduce into args)) ; <- poor man's union
+                    (partial merge-with +))
+        union-+-res (union-+ ^{:foo 154 :zip 19}       #{:a :b :c}
+                             ^{:foo 46 :bar 10}        #{1 42 187}
+                             ^{:bar 20 :is :untouched} #{1 :a})
+
+        conj-m-init (ff conj (partial merge {:foo 213, :a "egg"}))
+        conj-m-init-res (conj-m-init ^{:foo :bar, :baz 123} [1 2 3]
+                                     ^{:b 10}               [4]
+                                     ^{:doc "none"}         #{10})]
+    (testing "that ff applies data-fn correctly for multiple arguments"
+      (are [actual expected] (= actual expected)
+           conc-empty-res  [:a :b :c 1 42 187 5]
+           union-+-res     #{:a :b :c 1 42 187}
+           conj-m-init-res [1 2 3 [4] #{10}]))
+    
+    (testing "that ff applies metadata-fn correctly for multiple arguments"
+      (are [actual expected] (= (meta actual) expected)
+           conc-empty-res {}
+           union-+-res {:foo 200, :bar 30, :zip 19, :is :untouched}
+           conj-m-init-res {:foo :bar, :a "egg" :baz 123,
+                            :b 10, :doc "none"}))))
 
 (deftest test-meta-fm
   (let [r (fm right)
@@ -40,13 +67,13 @@
 
         mmerge (fm merge)
         mmerge-res (mmerge a b)]
-    (testing "that fm applies data-fn correctly"
+    (testing "that fm applies data-fn correctly for two args"
       (are [actual expected] (= actual expected)
            l-res a
            r-res b
            mmerge-res (merge a b)))
 
-    (testing "that fm merges metadata correctly"
+    (testing "that fm merges metadata correctly for two args"
       (are [res] (= (meta res) (merge (meta a) (meta b)))
            r-res
            l-res
@@ -75,9 +102,9 @@
            keep-right-right [a b c d]
            keep-left-left [d c b a]
            keep-right-left [d c b a])))
-  (let [a (with-meta [1] {:a 1})
-        b (with-meta {} {:a 2})
-        c (with-meta [2] {:a 2, :c :a})]
+  (let [a ^{:a 1} [1]
+        b ^{:a 2} {}
+        c ^{:a 2, :c :a} [2]]
     (testing "that duo-apply applies data-fn correctly for two elements"
       (are [actual expected] (= actual expected)
            (duo-apply [conj] [merge-with right]
