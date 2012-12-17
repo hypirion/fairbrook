f1aead880ff2c9e706f44d252f149865d5a50a73
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-12-17 22:44:26 +0100

    Make Travis happy.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://github.com/hyPiRion/fairbrook"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :aliases {"all" ["with-profile" "dev:1.2:1.3:1.5"]}
+  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-alpha4"]]}})
