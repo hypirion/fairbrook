ea91d4453f0ceee3e2aaa4abea57405b2614c3b0
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-12-22 02:54:07 +0100

    Add some training/side wheels for path-merge-with.
    Like, you know, there's something wrong with it.

--- test/fairbrook/test/path.clj
+++ test/fairbrook/test/path.clj
@@ -44,3 +44,36 @@
 
          {}
          [{1 0} {1 0}])))
+
+(deftest test-path-merge-with
+  (testing "that path-merge-with applies the default fn when merging non-rules"
+    (are [rules f maps expected]
+         (= (apply path-merge-with rules f maps) expected)
+
+         {} (fn [k v v2] [k v v2])
+         [{:a 10, :b :buxor} {:a :yxa} {:c :not-merged} {:b :xerxes}]
+         #_=> {:a [[:a] 10 :yxa], :b [[:b] :buxor :xerxes], :c :not-merged}
+
+         {} (fn [& r] r)
+         [{:a :this} {:a :is, :f :foo} {:a :nested}]
+         #_=> {:a '([:a] ([:a] :this :is) :nested), :f :foo}
+
+         {[:a] +, [:c] *} (fn [_ a b] (* 2 a b))
+         [{:a 10, :b 1} {:b 10, :c 9} {:a 11, :b 1, :c 3}]
+         #_=> {:a 21, :b 40, :c 27}
+
+         {[:a :b] +, [:a :c] *} (sub-merge-fn
+                                 (fn [k v1 v2]
+                                   (({[:a :b] +, [:a :c] *} k (fn [_ a] a))
+                                    v1 v2)))
+         [{:a {:b 10} :d {:woof true :bark false}} {:a {:b 2 :c 9}}
+          {:a {:c 4 :d 4, :b 3} :d {:bark true, :chasing :cat}}]
+         #_=> {:a {:b 15, :c 36, :d 4} :d {:woof true :bark true :chasing :cat}}
+
+         {} (sub-merge-fn
+             (fn [k v1 v2]
+               (({[:a :b] +, [:a :c] *} k (fn [_ a] a)) v1 v2)))
+         [{:a {:b 10} :d {:woof true :bark false}} {:a {:b 2 :c 9}}
+          {:a {:c 4 :d 4, :b 3} :d {:bark true, :chasing :cat}}]
+         #_=> {:a {:b 15, :c 36, :d 4}
+               :d {:woof true :bark true :chasing :cat}})))
