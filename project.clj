4f509bbc30b565dcac625bd38885909b710ac110
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-12-24 02:30:49 +0100

    Bump 1.5 to RC1.

--- project.clj
+++ project.clj
@@ -7,4 +7,4 @@
   :aliases {"all" ["with-profile" "dev:1.2:1.3:1.5"]}
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-beta2"]]}})
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}})
