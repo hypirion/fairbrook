6d588d8fb95efe9e9fb42ea688b404b7ed36c10a
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-01-04 03:02:54 +0100

    0.1.0 release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject fairbrook "0.1.0-SNAPSHOT"
+(defproject fairbrook "0.1.0"
   :description "Fine-grained map manipulation for the masses."
   :url "http://github.com/hyPiRion/fairbrook"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :aliases {"all" ["with-profile" "dev:1.2:1.3:1.5"]}
-  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :aliases {"all" ["with-profile" "dev:1.3:1.4:1.5"]}
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}})
