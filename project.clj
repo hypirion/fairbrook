488d481df93b678cbcb7e2faabe786335abef318
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-01-06 14:05:10 +0100

    Add codox plugin.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :aliases {"all" ["with-profile" "dev:1.3:1.4:1.5"]}
+  :plugins [[codox "0.6.4"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}})
