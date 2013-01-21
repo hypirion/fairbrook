6d722518877806cb28de6c3f6cfa072256efaff6
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-01-21 11:34:35 +0100

    Separate correctness tests and benchmarking tests.

--- project.clj
+++ project.clj
@@ -4,11 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.0"]]
-  :aliases {"all" ["with-profile" "dev:1.3:1.4:1.5"]}
+  :aliases {"all" ["with-profile" "1.2:1.3:1.4:1.5"]}
   :plugins [[codox "0.6.4"]]
   :test-selectors {:default (complement :benchmark)
                    :benchmark :benchmark}
-  :profiles {:dev {:dependencies [[criterium "0.3.1"]]}
+  :test-paths ["test/correctness"]
+  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC2"]]}})
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC2"]]}
+             :dev {:dependencies [[criterium "0.3.1"]
+                                  [org.clojure/clojure "1.4.0"]]
+                   :test-paths ["test/benchmark"]}})
